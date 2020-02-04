c622f5fd2f2e3f2d036a4cd9094f99110b5ea4bf
IngCristianGlez <54915766+IngCristianGlez@users.noreply.github.com>
2020-02-04 14:48:20 -0600

    First Lunch

---
+++ Logs/Logs/Migrations/20200203212043_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Logs.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Log",
+                columns: table => new
+                {
+                    idLog = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                        .Annotation("MySql:CharSet", "latin1")
+                        .Annotation("MySql:Collation", "latin1_swedish_ci"),
+                    Date_Register = table.Column<string>(type: "varchar(50)", nullable: false)
+                        .Annotation("MySql:CharSet", "latin1")
+                        .Annotation("MySql:Collation", "latin1_swedish_ci")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.idLog);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Log");
+        }
+    }
+}
---
+++ Logs/Logs/Models/Log.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Logs.Models
+{
+    public partial class Log
+    {   
+        [Key]
+        public int IdLog { get; set; }
+        public string Description { get; set; }
+        public string DateRegister { get; set; }
+    }
+}
